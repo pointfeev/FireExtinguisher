89dadf2691be625774b994538a5d9d66f7866153
pointfeev <zehmeen@gmail.com>
2021-08-20 11:40:24 -0400

    Improved mod compatibility method

--- 1.2/CompatibilityUtils.cs
+++ 1.2/CompatibilityUtils.cs
@@ -1,26 +1,52 @@
-﻿using System.Linq;
+﻿using HarmonyLib;
+using System;
+using System.Linq;
+using System.Reflection;
 using Verse;
 
 namespace FireExtinguisher
 {
     public static class CompatibilityUtils
     {
-        public static bool CombatExtendedInstalled = (from mod in ModLister.AllInstalledMods
-                                                      where mod.Active && mod.PackageId.ToLower() == "ceteam.combatextended"
-                                                      select mod).Any<ModMetaData>();
+        public static bool CombatExtendedIsActive;
+        public static MethodInfo combatExtendedHasAmmo_Method;
 
-        public static bool CombatExtendedAmmoCheck(ThingWithComps thingWithComps)
+        public static bool IsMethodCorrect(string modName, MethodInfo methodInfo, Type[] correctTypes)
         {
-            if (CombatExtendedInstalled) { return DoCombatExtendedAmmoCheck(thingWithComps); }
+            Type[] currentTypes = methodInfo.GetParameters().Select(pi => pi.ParameterType.IsByRef ? pi.ParameterType.GetElementType() : pi.ParameterType).ToArray();
+            for (int i = 0; i < currentTypes.Length; i++)
+            {
+                if (currentTypes[i] != correctTypes[i])
+                {
+                    Log.Error($"Colony Groups failed to support {modName}: Incorrect parameter {i + 1} for method '{methodInfo.ReflectedType.FullName + "." + methodInfo.Name}'!  Please report this error!" +
+                        "\n    " + currentTypes[i] + " != " + correctTypes[i]);
+                    return false;
+                }
+            }
             return true;
         }
 
-        // to prevent BreadthFirstTraverse error
-        private static bool DoCombatExtendedAmmoCheck(ThingWithComps thingWithComps)
+        static CompatibilityUtils()
         {
-            CombatExtended.CompAmmoUser comp = thingWithComps.TryGetComp<CombatExtended.CompAmmoUser>();
-            if (!(comp is null))
-                return !comp.UseAmmo || comp.CurMagCount > 0 || comp.HasAmmo;
+            CombatExtendedIsActive = ModLister.AllInstalledMods.Any(x => x.Active && x.PackageId.ToLower() == "ceteam.combatextended");
+            if (CombatExtendedIsActive)
+            {
+                combatExtendedHasAmmo_Method = AccessTools.Method(AccessTools.TypeByName("CombatExtended.CE_Utility"), "HasAmmo");
+                if (combatExtendedHasAmmo_Method is null)
+                {
+                    Log.Error("[FireExtinguisher] Failed to support Combat Extended! Please report this error!");
+                    CombatExtendedIsActive = false;
+                }
+                else
+                {
+                    CombatExtendedIsActive = IsMethodCorrect("Combat Extended", combatExtendedHasAmmo_Method, new Type[] { typeof(ThingWithComps) });
+                }
+            }
+        }
+
+        public static bool HasAmmo(ThingWithComps thingWithComps)
+        {
+            if (CombatExtendedIsActive) return (bool)combatExtendedHasAmmo_Method.Invoke(null, new object[] { thingWithComps });
             return true;
         }
     }
