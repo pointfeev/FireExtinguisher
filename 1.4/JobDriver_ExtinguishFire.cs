00eec37d2c6e9c71338b7533dc5df54336e1d799
pointfeev <zehmeen@gmail.com>
2023-01-17 15:15:16 -0500

    reorganize job driver

--- 1.4/JobDriver_ExtinguishFire.cs
+++ 1.4/JobDriver_ExtinguishFire.cs
@@ -1,36 +1,45 @@
 ﻿using System.Collections.Generic;
 using RimWorld;
+using Verse;
 using Verse.AI;
 
 namespace FireExtinguisher
 {
     public class JobDriver_ExtinguishFire : JobDriver
     {
-        private Fire TargetFire => (Fire)job.targetA.Thing;
+        private const TargetIndex FireIndex = TargetIndex.A;
 
         public override bool TryMakePreToilReservations(bool errorOnFailed)
-            => Map.reservationManager.CanReserve(pawn, TargetFire) && pawn.Reserve(TargetFire, job);
+        {
+            Fire fire = (Fire)job.GetTarget(FireIndex);
+            return fire != null && Map.reservationManager.CanReserve(pawn, fire) && pawn.Reserve(fire, job);
+        }
+
+        private Toil CheckDestroyedToil(TargetIndex targetIndex)
+        {
+            Toil toil = ToilMaker.MakeToil();
+            toil.initAction = delegate
+            {
+                Fire fire = (Fire)pawn.CurJob.GetTarget(targetIndex);
+                if (fire != null && !fire.Destroyed)
+                    return;
+                pawn.records.Increment(RecordDefOf.FiresExtinguished);
+                pawn.jobs.EndCurrentJob(JobCondition.Succeeded);
+            };
+            return toil;
+        }
 
         protected override IEnumerable<Toil> MakeNewToils()
         {
-            Toil checkDestroyed = new Toil
-            {
-                initAction = delegate
-                {
-                    if (TargetFire != null && !TargetFire.Destroyed)
-                        return;
-                    pawn.records.Increment(RecordDefOf.FiresExtinguished);
-                    pawn.jobs.EndCurrentJob(JobCondition.Succeeded);
-                }
-            };
             Toil equip = InventoryUtils.EquipFireExtinguisherToil();
-            _ = equip.JumpIfDespawnedOrNull(TargetIndex.A, checkDestroyed);
-            yield return Toils_Combat.TrySetJobToUseAttackVerb(TargetIndex.A);
-            Toil approach = CastUtils.GotoCastPosition(TargetIndex.A);
-            _ = approach.JumpIfDespawnedOrNull(TargetIndex.A, checkDestroyed);
+            _ = equip.EndOnDespawnedOrNull(FireIndex);
+            yield return Toils_Combat.TrySetJobToUseAttackVerb(FireIndex);
+            Toil checkDestroyed = CheckDestroyedToil(FireIndex);
+            Toil approach = CastUtils.GotoCastPosition(FireIndex);
+            _ = approach.JumpIfDespawnedOrNull(FireIndex, checkDestroyed);
             yield return approach;
-            Toil castVerb = Toils_Combat.CastVerb(TargetIndex.A);
-            _ = castVerb.JumpIfDespawnedOrNull(TargetIndex.A, checkDestroyed);
+            Toil castVerb = Toils_Combat.CastVerb(FireIndex);
+            _ = castVerb.JumpIfDespawnedOrNull(FireIndex, checkDestroyed);
             yield return castVerb;
             yield return checkDestroyed;
             yield return Toils_Jump.Jump(equip);
